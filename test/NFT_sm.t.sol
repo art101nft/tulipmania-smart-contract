bcd69c644161292fd23d197a7584f45ea5926e87
lza_menace <lza_menace@protonmail.com>
2022-11-26 23:04:20 -0800

    add basic stuff

---
+++ test/NFT_sm.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/NFT_sm.sol";
+
+contract NFT_sm is Test {
+    NFT public nft;
+
+    function setUp() public {
+        nft = new NFT("test", "test");
+    }
+
+    function testMint() public {
+        assertEq(nft.totalSupply(), 0);
+        vm.prank(address(0xdead));
+        nft.mint();
+        assertEq(nft.totalSupply(), 1);
+    }
+}
