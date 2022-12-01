c54184a74b0311a79e6bb0d8c95b82a51efcb58c
lza_menace <lza_menace@protonmail.com>
2022-12-01 01:24:55 -0800

    send txes

--- generate_commands.py
+++ generate_commands.py
@@ -2,14 +2,8 @@
 
 import json
 from web3 import Web3
-from os import getenv, path, walk
+from os import getenv, path
 from glob import glob
-
-# forge create NFT --rpc-url=$LOCAL_RPC --private-key=$LOCAL_KEY --constructor-args NFT NFT
-# cast send --rpc-url=$LOCAL_RPC --private-key=$LOCAL_KEY $CONTRACT "updateTulipData(string[])"
-
-WEB3_PROVIDER_URI = f'http://127.0.0.1:8545'
-w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URI))
 
 def get_eth_contract(_ca, _rp):
     compiled_contract_path = path.abspath(_rp)
@@ -19,7 +13,6 @@ def get_eth_contract(_ca, _rp):
         contract_abi = contract_json['abi']
 
     return w3.eth.contract(address=w3.toChecksumAddress(_ca), abi=contract_abi)
-
 
 def get_palette_data():
     codes = []
@@ -50,31 +43,45 @@ def get_tulip_data():
             tulips.append(_f)
     return tulips
 
+def sendit(w3, t, nonce):
+    t['from'] = w3.eth.defaultAccount
+    t['nonce'] = nonce
+    nonce += 1
+    s = w3.eth.account.sign_transaction(t, private_key=getenv('TESTNET_KEY'))
+    w3.eth.send_raw_transaction(s.rawTransaction)
+    return s.hash
 
 if __name__ == '__main__':
-    if not getenv('CONTRACT'):
-        print('CONTRACT not specified')
-        exit()
-    if not getenv('LOCAL_KEY'):
-        print('LOCAL_KEY not specified')
+    if not getenv('TESTNET_CONTRACT') or not getenv('TESTNET_KEY') or not getenv('TESTNET_RPC'):
+        print('invalid env vars set')
         exit()
     
-    w3.eth.defaultAccount = w3.eth.account.from_key(getenv('LOCAL_KEY')).address
+    WEB3_PROVIDER_URI = getenv('TESTNET_RPC')
+    w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URI))
+    w3.eth.defaultAccount = w3.eth.account.from_key(getenv('TESTNET_KEY')).address
     palette_data = get_palette_data()
     symbol_data = get_symbol_data()
     tulip_data = get_tulip_data()
-    contract = get_eth_contract(getenv('CONTRACT'), './out/NFT.sol/NFT.json')
+    contract = get_eth_contract(getenv('TESTNET_CONTRACT'), './out/NFT.sol/NFT.json')
 
-    total_gas = []
-    r = contract.functions.updatePaletteData(palette_data).estimate_gas()
-    print(f'{r} gas to publish palette data')
-    total_gas.append(r)
+    # total_gas = []
+    # r = contract.functions.updatePaletteData(palette_data).estimate_gas()
+    # total_gas.append(r)
+    # for i in symbol_data:
+    #     r = contract.functions.updateSymbolData(i, symbol_data[i]).estimate_gas()
+    #     total_gas.append(r)
+    # r = contract.functions.updateTulipData(tulip_data).estimate_gas()
+    # total_gas.append(r)
+
+    nonce = w3.eth.get_transaction_count(w3.eth.defaultAccount)
+
+    r = sendit(w3, contract.functions.updatePaletteData(palette_data).build_transaction(), nonce)
+    print(f'sent tx {r} with nonce {nonce}')
+    nonce += 1
     for i in symbol_data:
-        r = contract.functions.updateSymbolData(i, symbol_data[i]).estimate_gas()
-        print(f'{r} gas to publish symbol data ({i})')
-        total_gas.append(r)
-    r = contract.functions.updateTulipData(tulip_data).estimate_gas()
-    print(f'{r} gas to publish tulip data')
-    total_gas.append(r)
-
-    print(f'\nTotal gas required to publish {len(palette_data)} color palettes, {len(symbol_data)} symbols, and {len(tulip_data)} tulip pieces:\n{sum(total_gas)}')
+        r = sendit(w3, contract.functions.updateSymbolData(i, symbol_data[i]).build_transaction(), nonce)
+        print(f'sent tx {r} with nonce {nonce}')
+        nonce += 1
+    r = sendit(w3, contract.functions.updateTulipData(tulip_data).build_transaction(), nonce)
+    print(f'sent tx {r} with nonce {nonce}')
+    nonce += 1
